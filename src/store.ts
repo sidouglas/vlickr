19a9d3d8f0ebce413062fb59eedfb96a6705f3fe
sidouglas <1107946+sidouglas@users.noreply.github.com>
2024-06-08 22:05:25 +1000

    Update zustand store

--- src/store.ts
+++ src/store.ts
@@ -1,27 +1,75 @@
 import { create } from 'zustand';
 import { devtools, subscribeWithSelector } from 'zustand/middleware';
 import { FlickrImage } from './types/flickrImage';
+import { urlEncode } from './utils';
+import { PER_PAGE } from './constants';
+
+type Pagination = {
+  nextPageUrl: string;
+  page: number;
+  pages: number;
+  perpage: number;
+  prevPageUrl: string;
+  total: number;
+};
+
+type NonFunctionKeys<T> = {
+  [K in keyof T]: T[K] extends (...args: never[]) => unknown ? never : K;
+}[keyof T];
 
 export type Store = {
-  currentPage: number;
+  error: string | null;
+  isLoading: boolean;
+  pagination: Pagination | null;
   photos: FlickrImage[];
   searchTerm: string;
-  error: string | null;
-  setPhotos: (photos: FlickrImage[]) => void;
   setError: (error: string | null) => void;
+  setIsLoading: (isLoading: boolean) => void;
+  setPagination: (pagination: Partial<Pagination>) => void;
+  setPhotos: (photos: FlickrImage[]) => void;
   setSearchTerm: (value: string) => void;
+};
+
+export const defaultState: Pick<Store, NonFunctionKeys<Store>> = {
+  error: null,
+  isLoading: false,
+  pagination: null,
+  photos: [],
+  searchTerm: '',
 };
 
 export const useStore = create<Store>()(
   devtools(
-    subscribeWithSelector((set) => ({
-      currentPage: 1,
-      photos: [],
-      searchTerm: '',
-      error: null,
+    subscribeWithSelector((set, get) => ({
+      ...defaultState,
+      setError: (error) => set({ error }),
+      setIsLoading: (isLoading) => set({ isLoading }),
+      setPagination: (pagination = {}) => {
+        set((state) => {
+          const { page, pages, perpage, total } = pagination;
+          const currentPage = page ?? state.pagination?.page ?? 1;
+          const itemsPerPage = perpage ?? state.pagination?.perpage ?? PER_PAGE;
+          const totalCount = total ?? state.pagination?.total ?? 1;
+          const totalPages = pages ?? state.pagination?.pages ?? 1;
+
+          const encodedSearchTerm = urlEncode(state.searchTerm);
+          const nextPage = Math.min((currentPage ?? 0) + 1, totalPages);
+          const prevPage = Math.max((currentPage ?? 1) - 1, 1);
+
+          return {
+            pagination: {
+              nextPageUrl: `/search/${nextPage}?search=${encodedSearchTerm}`,
+              page: currentPage,
+              pages: totalPages,
+              perpage: itemsPerPage,
+              prevPageUrl: `/search/${prevPage}?search=${encodedSearchTerm}`,
+              total: totalCount,
+            },
+          };
+        });
+      },
       setPhotos: (photos) => set({ photos }),
       setSearchTerm: (searchTerm) => set({ searchTerm }),
-      setError: (error) => set({ error }),
     }))
   )
 );
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { ClassNameValue, twMerge } from 'tailwind-merge';
 
-export function getAspectRatio(w: number | string, h: number | string): string {
+export function getAspectRatio(w: number | string, h: number | string): [string, number, number] {
   const width = Number(w);
   const height = Number(h);
   // Calculate the greatest common divisor (GCD) of width and height
@@ -11,10 +11,17 @@ export function getAspectRatio(w: number | string, h: number | string): string {
   const aspectWidth: number = width / divisor;
   const aspectHeight: number = height / divisor;
 
-  // Return the aspect ratio as a string
-  return `${aspectWidth}/${aspectHeight}`;
+  return [`${aspectWidth}/${aspectHeight}`, aspectWidth, aspectHeight];
 }
 
 export function cn(...inputs: ClassNameValue[]) {
   return twMerge(...inputs);
+}
+
+export function urlEncode(str: string): string {
+  return encodeURIComponent(str).replace(/%20/g, '+');
+}
+
+export function urlDecode(str: string): string {
+  return decodeURIComponent(str.replace(/\+/g, '%20'));
 }
